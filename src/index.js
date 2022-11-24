6663703186697f903c647a81b58ae689f4da1245
Betsy <89090441+BetsyAlejandra@users.noreply.github.com>
2022-11-23 23:58:52 -0500

    Chat

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { MainApp } from './pages/MainApp';
 import { BrowserRouter } from 'react-router-dom';
+import { OpcionesRegistro } from './pages/OpcionesRegistro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/'>
-    <MainApp />
+  <BrowserRouter>
+  <OpcionesRegistro />
   </BrowserRouter>
 );
 
--- src/pages/OpcionesRegistro.jsx
+++ src/pages/OpcionesRegistro.jsx
@@ -1,55 +1,23 @@
-import { NavBarD } from '../share/NavBarD'
+import { NavBarD } from "../share/NavBarD"
 import './OpcionesRegistro.css'
-import React, { useState } from 'react'
-import { auth } from '../firebase'
 
 export const OpcionesRegistro = () => {
 
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-
-    const registrarUsuario = (e) => {
-        e.preventDefault()
-        auth.createUserWithEmailAndPassword(email, pass)
-        .then((res) => alert('Usuario Registrado'))
-        .catch(err => alert('La contraseña debe tener 6 o mas caracteres'));
-    }
-
+    
     return (
         <>
             <NavBarD />
+            <title className='h8'> Registrate </title>
             <div id='container'>
-                <div id='cardCentral'>
-                <title className='h8'> Registro </title>
-                    <form id='centrado' onSubmit={registrarUsuario}>
-                        <div className='inputs'>
-                            <label htmlFor="nombres" id='labelf'>
-                                Nombre </label> <br />
-                            <input type='text' id='inputf' name='nombres'
-                                placeholder='Patricia Maria' required></input> <br /> <br />
-                            <label htmlFor="apellidos" id='labelf'>
-                                Apellido </label> <br />
-                            <input type='text' id='inputf' name='apellidos'
-                                placeholder='Estrella Alvarez' required></input> <br /> <br />
-                            <label htmlFor="correo" id='labelf'>
-                                Correo </label> <br />
-                            <input onChange={(e) => {
-                                setEmail(e.target.value)
-                            }}
-                            type='email' id='inputf' name='correo'
-                                placeholder='hola@gmail.com' required></input> <br /> <br />
-                            <label htmlFor="contrasena" id='labelf'>
-                                Contraseña </label> <br />
-                            <input onChange={(e) => {
-                                setPass(e.target.value)
-                            }}
-                            type='password' id='inputf' name='contrasena'
-                                placeholder='********' required></input> <br />
-                            <label id='labeltyc'>
-                                <input type="checkbox" id="teyco" value="teyco"></input>
-                                Acepto los Terminos y Condiciones</label>
-                            <button className="btn btn-primary" id='boton' type="submit" value="Registrar"> Registrarse </button>
-                        </div>
+                <div id="cardCentral">
+                    <form id="centrado" action="">
+                    <label htmlFor="name" id='labelf'> Nombre </label> 
+                        <input type="name" name="correo" id="inputf" required /> <br />
+                        <label htmlFor="correo" id='labelf'> Correo </label> 
+                        <input type="email" name="correo" id="inputf" required /> <br />
+                        <label htmlFor="contrasena" id='labelf'> Contrasena </label> 
+                        <input type="password" name="contrasena" id="inputf" required/> <br /> <br />
+                        <button className="btn btn-primary" id='boton' type="submit"> Registrar </button> <br />
                     </form>
                 </div>
             </div>
