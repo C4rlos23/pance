47fa503dba077e265040410a7736e53124d3162f
Betsy <89090441+BetsyAlejandra@users.noreply.github.com>
2022-11-23 20:51:48 -0500

    Solo home

--- src/pages/MainApp.jsx
+++ src/pages/MainApp.jsx
@@ -1,43 +1,11 @@
 import React from "react";
 import {  Route, Routes } from "react-router-dom";
 import { Home } from "./Home"
-<<<<<<< HEAD
-=======
-import { Registro } from "./Registro";
-import { PaginaPrincipal } from "./PaginaPrincipal";
-import { OpcionesRegistro } from "./OpcionesRegistro";
-import { InicioSesion } from "./InicioSesion";
-import { Restaurantes } from "./Restaurantes";
-import { Compra } from "./Compra";
-import { Perfil } from "./Perfil";
-import { Canasta } from "./Canasta";
-import { Siguetu } from "./Siguetu"
-import { Chat } from "./Chat";
-import { Locales } from "./Locales";
-import { Productos } from "./Productos";
->>>>>>> 8aca73704e854c868a19c6c44c0cc5f1be8174f9
-
-
 
 export const MainApp = () => {
     return(
         <Routes>
             <Route path="/" element={ <Home /> }> </Route>
-<<<<<<< HEAD
-=======
-            <Route path="/registrocorreo" element={ <OpcionesRegistro /> }></Route>
-            <Route path="/opcionesregistro" element={ <Registro /> }></Route>
-            <Route path="/home" element={ <PaginaPrincipal/> }></Route>
-            <Route path="/iniciosesion" element={ <InicioSesion/> }></Route>
-            <Route path="/restaurantes" element={ <Restaurantes /> }></Route>
-            <Route path="/compra" element={ <Compra />}></Route>
-            <Route path="/perfil" element={ <Perfil />}></Route>
-            <Route path="/carrito" element={ <Canasta />}></Route>
-            <Route path="/siguetupedido" element={ <Siguetu />}></Route>
-            <Route path="/chat" element={ <Chat /> }></Route>
-            <Route path="/registrolocal" element={ <Locales /> }></Route>
-            <Route path="/compraproductos" element={ <Productos/> }></Route>
->>>>>>> 8aca73704e854c868a19c6c44c0cc5f1be8174f9
         </Routes>        
     )
 }
